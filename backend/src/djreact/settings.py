3f8b1d9ae1008e65e81dc830afd3110d1926011d
unknown <sonwj0915@naver.com>
2021-03-16 01:00:44 +0900

    django /api/에 데이터 가져오기

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
 
     'articles',
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djreact.urls'
@@ -129,3 +132,4 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+CORS_ORIGIN_ALLOW_ALL = True
--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import Articles from "../components/Article";
 
@@ -16,6 +17,19 @@ for (let i = 0; i < 23; i++) {
 }
 
 class ArticleList extends React.Component {
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/api/").then((res) => {
+      this.setState({
+        articles: res.data,
+      });
+      console.log(res.data);
+    });
+  }
+
   render() {
     return (
       <div>
