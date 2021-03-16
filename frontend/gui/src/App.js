641489d0a6e5f23234fadd966218252aaefca98b
unknown <sonwj0915@naver.com>
2021-03-16 14:26:38 +0900

    디테일뷰 페이지 만들기

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import BaseRouter from "./routes";
 import "antd/dist/antd.css";
 
 import CustumLayout from "./containers/Layout";
-import ArticleList from "./containers/ArticleListView";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CustumLayout></CustumLayout>
-        <ArticleList />
+        <Router>
+          <CustumLayout>
+            <BaseRouter />
+          </CustumLayout>
+        </Router>
       </div>
     );
   }
