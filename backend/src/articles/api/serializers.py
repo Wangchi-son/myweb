616e247fa40c30a1797801b7221af99f67708298
unknown <sonwj0915@naver.com>
2021-03-16 23:38:04 +0900

    api views, urls 간편하게 수정

--- backend/src/articles/api/serializers.py
+++ backend/src/articles/api/serializers.py
@@ -4,6 +4,9 @@ from articles.models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        many = kwargs.pop('many', True)
+        super(ArticleSerializer, self).__init__(many=many, *args, **kwargs)
 
     class Meta:
         model = Article
